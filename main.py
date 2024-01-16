46ea0111b26567c2da1b9b50c935dc1ccbd5387a
lequangkhai06 <khaidev.com@gmail.com>
2024-01-16 19:41:43 +0700

    modified:   main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-# -*- coding: utf-8 -*-
-
-# @autor: Matheus Felipe
-# @github: github.com/matheusfelipeog
-
-# Builtin
 import tkinter as tk
-
-# Módulo próprio
 from app.calculadora import Calculadora
 
 if __name__ == '__main__':
